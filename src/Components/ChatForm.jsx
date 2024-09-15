1b33c4b08564acb9edfdc588395a80803cb85baa
jorge <jresino@gmail.com>
2024-09-15 12:23:30 +0200

    erer

--- src/Components/ChatForm.jsx
+++ src/Components/ChatForm.jsx
@@ -18,27 +18,37 @@ function ImageButtonLabel(props) {
   );
 }
 
-function getIaAnswer(userMessage) {
-  const iaFakeAnswer = {
-    ia: "Eres tonto o que",
-    user: userMessage,
-  };
-
-  return iaFakeAnswer;
+async function getIaAnswer(userMessage) {
+  try {
+    const response = await fetch("https://reqres.in/api/users?page=2");
+    const responseText = await response.text();
+    console.log(`ASYNC fecth Response : ${responseText}`);
+    return responseText[0];
+  } catch (e) {
+    console.log(`ASYNC fetch Error API --> ${e}`);
+    return {
+      ia: "Eres tonto o que",
+      user: userMessage,
+    };
+  }
 }
 
 export function ChatForm() {
   const multipleFilesFlag = true;
   const [memory, setMemory] = useState("");
+  const [files, setFiles] = useState([]);
   return (
     <div>
       <form
         className="form"
         onSubmit={(e) => {
-            console.log(e);
+          console.log(e);
           e.preventDefault();
-          const iaAnswer = getIaAnswer(memory);
-          console.log(`User : ${iaAnswer.user} -- iaAnswer : ${iaAnswer.ia}`);
+          const iaAnswer = getIaAnswer(memory)
+            .then((result) => result)
+            .catch((e) => console.log(e));
+          // console.log(`User : ${iaAnswer.user} -- iaAnswer : ${iaAnswer.ia}`);
+          console.log(`API answwer : ${iaAnswer}`);
         }}
       >
         <ImageFileLabel htmlFor="inputFile" labelClassName="inputFileLabel" />
@@ -48,6 +58,12 @@ export function ChatForm() {
           accept="application/pdf"
           multiple={multipleFilesFlag}
           className="inputFile"
+          onChange={(e) => {
+            // e.target.files.forEach(file => setFiles(file))
+            setFiles([...files,e.target.files])
+            // setFiles([...filesFakePath, e.target.files]);
+            console.log(`files :${files}`);
+          }}
         />
         <input
           type="text"
@@ -66,11 +82,7 @@ export function ChatForm() {
           htmlFor="SubmitButton"
           labelClassName="submitButtonLabel"
         />
-        <button
-          type="submit"
-          id="SubmitButton"
-          className="SubmitButton"
-        />
+        <button type="submit" id="SubmitButton" className="SubmitButton" />
       </form>
     </div>
   );
