939f5f1b0d885006cc017613346c39fc524a1bd5
jorge <jresino@gmail.com>
2024-09-18 11:21:13 +0200

    card image

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,23 +2,26 @@ export function Card(props) {
   return (
     <>
       <div className="text-white font-mono text-xs max-w-max bg-[#acacac] items-center p-2 rounded-lg">
-        <div className="inline-block">
-          <h1 className="font-semibold text-center">
+        <div className="inline-block px-1 py-1">
+          <h1 className="font-semibold text-sm text-center mt-1 mb-4">
             {props.title ? props.title : ""}
           </h1>
           <p className="text-center hover:font-bold">
             {props.text ? props.text : ""}
           </p>
-          <a
-            href={props.href}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-        <props.icon
-            size={30}
-            color={props.iconColor}
-            className="mt-3 mx-auto hover:scale-125 transition transform duration-200"
-          />
+          <a href={props.href} target="_blank" rel="noopener noreferrer">
+            {props.icon ? (
+              <props.icon
+                size={35}
+                color={props.iconColor}
+                className="mt-4 mb-2 mx-auto my-2 hover:scale-125 transition transform duration-200"
+              />
+            ) : (
+              <img
+                src={props.src}
+                className="mt-4 mb-2 my-2 mx-auto hover:scale-110 transition transform duration-200"
+              />
+            )}
           </a>
         </div>
       </div>
--- src/Components/DevPage.jsx
+++ src/Components/DevPage.jsx
@@ -4,6 +4,9 @@ import { SiVite } from "react-icons/si";
 import { SiFastapi } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaNodeJs } from "react-icons/fa";
+import { SiLangchain } from "react-icons/si";
+import { SiHuggingface } from "react-icons/si";
+import pineconeLogoSrc from "../assets/pinecone_logo.jpg";
 
 function DevPage() {
   return (
@@ -11,23 +14,23 @@ function DevPage() {
       <div className="h-min w-auto rounded-lg grid grid-cols-7 gap-4 px-4 py-4">
         <Card
           href="https://es.react.dev/"
-          title="React"
+          title="React JS"
           iconColor="#3adee4"
-          text="UI web or Front-end built using this JavaScript library."
+          text="A JavaScript library used to build the UI web or Front-end of an App."
           icon={FaReact}
         />
         <Card
           href="https://vitejs.dev/guide/why.html"
           title="Vite"
           iconColor="#e43ae4"
-          text="Built with this tool that allows configuring a development environment for Vue, TezJS, and React frameworks."
+          text="A tool that allows configuring a development environment for Vue, TezJS, and React frameworks."
           icon={SiVite}
         />
         <Card
           href="https://nodejs.org/en/about"
           title="Node.js"
           iconColor="#31b13c"
-          text="Built using this runtime environment that allows JavaScript to be executed on the server side."
+          text="A runtime environment that allows JavaScript to be executed on the server side."
           icon={FaNodeJs}
         />
         <Card
@@ -41,8 +44,32 @@ function DevPage() {
           href="https://fastapi.tiangolo.com/"
           title="FastApi"
           iconColor="#009688 "
-          text="Back-end developed using this high-performance, web framework for building APIs with Python."
+          text="A Python web framework for building APIs which allows high-performance Back-End developing and host the agentic LLM graph that process the user querys."
           icon={SiFastapi}
+        />
+        <Card
+          href="https://www.langchain.com/"
+          title="LangChain and LangGraph"
+          iconColor="#0052CC"
+          text="LangChain is a framework to build with LLMs 
+          by chaining interoperable components. LangGraph is the framework for building controllable agentic workflows which is running on the Back-End"
+          icon={SiLangchain}
+        />
+        <Card
+          href="https://huggingface.co/"
+          title="HuggingFace"
+          iconColor="#FFD700"
+          text="Hugging Face is a machine learning (ML) and data science platform and community that helps users build, deploy and train machine 
+          learning models. It provides the infrastructure to demo, run and deploy artificial intelligence (AI) in live applications"
+          icon={SiHuggingface}
+        />
+        <Card
+          href="https://www.pinecone.io/"
+          title="Pinecone Vector DataBase"
+          text="AI is a leading provider of vector database technology, 
+          offering a fully managed, scalable, and highly performant platform for storing, indexing, and querying vector data."
+          src={pineconeLogoSrc}
+          icon={false}
         />
       </div>
     </main>
